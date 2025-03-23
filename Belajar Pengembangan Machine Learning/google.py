c1db5d7ecc6ca88dbbd3425c1a8283df520aa3b3
miezlearning <gottfriedemptiness@gmail.com>
2025-03-23 13:25:19 +0800

    refactor: update file path handling for saving WhatsApp reviews CSV

--- Belajar Pengembangan Machine Learning/google.py
+++ Belajar Pengembangan Machine Learning/google.py
@@ -1,6 +1,7 @@
 from google_play_scraper import reviews
 import pandas as pd
-import os
+from pathlib import Path
+
 result, _ = reviews(
     'com.whatsapp',  
     lang='id',       
@@ -9,7 +10,7 @@ result, _ = reviews(
 )
 
 df = pd.DataFrame(result)
-base_dir = '/C:/Users/Miez/Documents/Dicoding/Belajar Pengembangan Machine Learning/data'
-os.makedirs(base_dir, exist_ok=True)
-csv_path = os.path.join(base_dir, 'whatsapp_reviews.csv')
+base_dir = Path.home() / 'Documents' / 'Dicoding' / 'Belajar Pengembangan Machine Learning' / 'data'
+base_dir.mkdir(parents=True, exist_ok=True)
+csv_path = base_dir / 'whatsapp_reviews.csv'
 df.to_csv(csv_path, index=False)
