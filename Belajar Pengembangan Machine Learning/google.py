3e268cb92976626af39b997427299af1fff51515
miezlearning <gottfriedemptiness@gmail.com>
2025-03-23 13:23:58 +0800

    refactor: increase review count to 12000 and save CSV to specified directory

--- Belajar Pengembangan Machine Learning/google.py
+++ Belajar Pengembangan Machine Learning/google.py
@@ -1,11 +1,15 @@
 from google_play_scraper import reviews
-result, _ = reviews(
-    'com.whatsapp',  # ID aplikasi WhatsApp
-    lang='id',       # Bahasa Indonesia
-    country='id',    # Negara Indonesia
-    count=3000       # Jumlah ulasan
-)
-# Simpan ke file CSV
 import pandas as pd
+import os
+result, _ = reviews(
+    'com.whatsapp',  
+    lang='id',       
+    country='id',    
+    count=12000       
+)
+
 df = pd.DataFrame(result)
-df.to_csv('whatsapp_reviews.csv', index=False)
+base_dir = '/C:/Users/Miez/Documents/Dicoding/Belajar Pengembangan Machine Learning/data'
+os.makedirs(base_dir, exist_ok=True)
+csv_path = os.path.join(base_dir, 'whatsapp_reviews.csv')
+df.to_csv(csv_path, index=False)
