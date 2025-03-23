ec5bfc170bfde86d3702afa48abeff976ffaca35
miezlearning <gottfriedemptiness@gmail.com>
2025-03-23 14:21:16 +0800

    refactor: update review scraper to target Mobile Legends and increase count to 50000

--- Belajar Pengembangan Machine Learning/google.py
+++ Belajar Pengembangan Machine Learning/google.py
@@ -3,14 +3,14 @@ import pandas as pd
 from pathlib import Path
 
 result, _ = reviews(
-    'com.whatsapp',  
+    'com.mobile.legends',  
     lang='id',       
     country='id',    
-    count=21000       
+    count=50000       
 )
 
 df = pd.DataFrame(result)
 base_dir = Path.home() / 'Documents' / 'Dicoding' / 'Belajar Pengembangan Machine Learning' / 'data'
 base_dir.mkdir(parents=True, exist_ok=True)
-csv_path = base_dir / 'whatsapp_reviews.csv'
+csv_path = base_dir / 'mobellejen.csv'
 df.to_csv(csv_path, index=False)
