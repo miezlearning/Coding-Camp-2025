0078e9172dfd1d009446f30151115b53e67c1003
miezlearning <gottfriedemptiness@gmail.com>
2025-03-25 01:55:07 +0800

    refactor: simplify review scraping to a single app and increase review count to 15000

--- Belajar Pengembangan Machine Learning/cari_ulasan.py
+++ Belajar Pengembangan Machine Learning/cari_ulasan.py
@@ -2,7 +2,7 @@ from google_play_scraper import reviews
 import pandas as pd
 from pathlib import Path
 
-app_ids = ['com.miHoYo.GenshinImpact', 'com.ncsoft.lineagew', 'com.com2us.smon.normal.freefull.google.kr.android.common']  
+app_id = 'com.miHoYo.GenshinImpact'  # Genshin Impact app ID
 
 def scrape_reviews(app_id, count=1000):
     result, _ = reviews(
@@ -13,15 +13,12 @@ def scrape_reviews(app_id, count=1000):
     )
     return result
 
-all_reviews = []
-for app_id in app_ids:
-    reviews_data = scrape_reviews(app_id, count=5000)  
-    all_reviews.extend(reviews_data)
+reviews_data = scrape_reviews(app_id, count=15000)  
 
-df = pd.DataFrame(all_reviews)
+df = pd.DataFrame(reviews_data)
 base_dir = Path.home() / 'Documents' / 'Dicoding' / 'Belajar Pengembangan Machine Learning' / 'data'
-base_dir.mkdir(parents=True, exist_ok=True)  # Membuat folder jika belum ad
-csv_path = base_dir / 'hasil_review.csv'
+base_dir.mkdir(parents=True, exist_ok=True)  # Membuat folder jika belum ada
+csv_path = base_dir / 'hasil_review_genshin_impact.csv'
 df.to_csv(csv_path, index=False)
 
 print(f"Data ulasan telah disimpan di: {csv_path}")
